b2dd980b77d6af91d5987300c93e66b1e6a7819b
monstruosoft <monstruosoft@users.noreply.github.com>
2017-10-14 18:18:44 -0500

    Nueva clase TareasProgramacion
    Se agregó la clase TareasProgramacion con el método cantidadConLetra() para convertir números a su representación en forma de texto.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # java-utils
 Utilidades Java de monstruosoft
+
+### Contenido del repositorio
+
+#### Paquete *org.monstruosoft.utils*
+* Clase **TareasProgramación**
+  * Método **cantidadConLetra()** - Convierte un número a su representación en forma de texto
+
+
+
+###### Última modificación
+sáb oct 14 18:00:14 CDT 2017
---
+++ org/monstruosoft/utils/TareasProgramacion.java
@@ -0,0 +1,165 @@
+/**
+ * @file TareasProgramacion.java
+ * 
+ * @author monstruosoft
+ * 
+ * @section LICENSE License
+ * 
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ * 
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * For more information, please refer to <https://unlicense.org>
+ * 
+ * @section DESCRIPTION_EN Description
+ * 
+ * This file contains sample Java homework code meant to be used for 
+ * educational purposes and may contain text and/or comments written 
+ * mainly in spanish.
+ * 
+ * @section DESCRIPTION_ES Descripción
+ * 
+ * Este archivo contiene código de demostración para tareas típicas de 
+ * programación en Java para su uso con fines educativos.
+ */
+package org.monstruosoft.utils;
+
+
+
+// Para el formato de cantidades
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+
+
+public class TareasProgramacion {
+    
+    /**
+     * Convierte el número que recibe como argumento a su representación escrita con letra.
+     * 
+     * @param s Una cadena de texto que contiene los dígitos de un número.
+     * @return  Una cadena de texto que contiene la representación con letra de
+     *          la parte entera del número que se recibió como argumento.
+     */
+    public static String cantidadConLetra(String s) {
+        StringBuilder result = new StringBuilder();
+        BigDecimal totalBigDecimal = new BigDecimal(s).setScale(2, BigDecimal.ROUND_DOWN);
+        long parteEntera = totalBigDecimal.toBigInteger().longValue();
+        int triUnidades      = (int)((parteEntera % 1000));
+        int triMiles         = (int)((parteEntera / 1000) % 1000);
+        int triMillones      = (int)((parteEntera / 1000000) % 1000);
+        int triMilMillones   = (int)((parteEntera / 1000000000) % 1000);
+        
+        if (parteEntera == 0) {
+            result.append("Cero ");
+            return result.toString();
+        }
+        
+        if (triMilMillones > 0) result.append(triTexto(triMilMillones).toString() + "Mil ");
+        if (triMillones > 0)    result.append(triTexto(triMillones).toString());
+        
+        if (triMilMillones == 0 && triMillones == 1) result.append("Millón ");
+        else if (triMilMillones > 0 || triMillones > 0) result.append("Millones ");
+        
+        if (triMiles > 0)       result.append(triTexto(triMiles).toString() + "Mil ");
+        if (triUnidades > 0)    result.append(triTexto(triUnidades).toString());
+        
+        return result.toString();
+    }
+    
+    
+    
+    /**
+     * Convierte una cantidad de tres cifras a su representación escrita con letra.
+     * 
+     * @param n La cantidad a convertir.
+     * @return  Una cadena de texto que contiene la representación con letra 
+     *          del número que se recibió como argumento.
+     */
+    private static StringBuilder triTexto(int n) {
+        StringBuilder result = new StringBuilder();
+        int centenas = n / 100;
+        int decenas  = (n % 100) / 10;
+        int unidades = (n % 10);
+        
+        switch (centenas) {
+            case 0: break;
+            case 1:
+                if (decenas == 0 && unidades == 0) {
+                    result.append("Cien ");
+                    return result;
+                }
+                else result.append("Ciento ");
+                break;
+            case 2: result.append("Doscientos "); break;
+            case 3: result.append("Trescientos "); break;
+            case 4: result.append("Cuatrocientos "); break;
+            case 5: result.append("Quinientos "); break;
+            case 6: result.append("Seiscientos "); break;
+            case 7: result.append("Setecientos "); break;
+            case 8: result.append("Ochocientos "); break;
+            case 9: result.append("Novecientos "); break;
+        }
+        
+        switch (decenas) {
+            case 0: break;
+            case 1:
+                if (unidades == 0) { result.append("Diez "); return result; }
+                else if (unidades == 1) { result.append("Once "); return result; }
+                else if (unidades == 2) { result.append("Doce "); return result; }
+                else if (unidades == 3) { result.append("Trece "); return result; }
+                else if (unidades == 4) { result.append("Catorce "); return result; }
+                else if (unidades == 5) { result.append("Quince "); return result; }
+                else result.append("Dieci");
+                break;
+            case 2:
+                if (unidades == 0) { result.append("Veinte "); return result; }
+                else result.append("Veinti");
+                break;
+            case 3: result.append("Treinta "); break;
+            case 4: result.append("Cuarenta "); break;
+            case 5: result.append("Cincuenta "); break;
+            case 6: result.append("Sesenta "); break;
+            case 7: result.append("Setenta "); break;
+            case 8: result.append("Ochenta "); break;
+            case 9: result.append("Noventa "); break;
+        }
+        
+        if (decenas > 2 && unidades > 0)
+            result.append("y ");
+        
+        switch (unidades) {
+            case 0: break;
+            case 1: result.append("Un "); break;
+            case 2: result.append("Dos "); break;
+            case 3: result.append("Tres "); break;
+            case 4: result.append("Cuatro "); break;
+            case 5: result.append("Cinco "); break;
+            case 6: result.append("Seis "); break;
+            case 7: result.append("Siete "); break;
+            case 8: result.append("Ocho "); break;
+            case 9: result.append("Nueve "); break;
+        }
+        
+        return result;
+    }
+}
